e9164ebfb15c00767f4fdeecb18b97b6384d51a2
Alejandro Fernandez <alejandro.fernandezr@perficient.com>
2023-04-18 09:37:11 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+
+
+/* /* GET home page. 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router; */
+
+const express = require("express");
+const router = express.Router();
+const bicicletaController = require("../controllers/bicicleta");
+
+router.get("/", bicicletaController.list);
+router.get("/bicicletas/:id/show", bicicletaController.show);
+router.get("/bicicletas/create", bicicletaController.create_get);
+router.post("/create", bicicletaController.create_post);
+router.get("/bicicletas/:id/update", bicicletaController.update_get);
+router.post("/:id/update", bicicletaController.update_post);
+router.post("/bicicletas/:id/delete", bicicletaController.delete);
+
+module.exports = router;
